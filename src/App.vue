5056a42ce84a79bcf8b7f6d84f7fa2f76919754f
thatcatinsomnia <thatcatinsomnia@gmail.com>
2020-04-02 22:33:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div class="desktop"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style lang="scss"></style>
